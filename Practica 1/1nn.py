8faedf5ef30f042a0f8ea289840bd9ba094f36df
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2018-03-14 15:51:48 +0100

    Añadida la función de lectura de datos

---
+++ Practica 1/1nn.py
@@ -0,0 +1,19 @@
+data = []
+
+def lecturaDatos(nombre_fich):
+    f = open(nombre_fich, "r")
+    #El siguiente booleano indica cuando empiezan los datos en el fichero.
+    son_datos = False
+    linea_data = False
+    for linea in f:
+        #Tenemos en cuenta la aparición de @data y cogemos la línea de después.
+        if son_datos:
+            linea_data=True
+        if "@data" in linea:
+            son_datos=True
+        if linea_data:
+            data.append((linea.rstrip()).split(","))
+    print(data[0])
+
+fichero = input("Introduzca la dirección al fichero de datos: ")
+lecturaDatos(fichero)
