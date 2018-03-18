783cac518ee84e283e3d9cbbed15fe0ea641b91f
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2018-03-18 18:11:46 +0100

    Añadida función de distancia euclidea

--- Practica 1/auxiliar.py
+++ Practica 1/auxiliar.py
@@ -1,6 +1,7 @@
-data = []
+import math
 
 def lecturaDatos(nombre_fich):
+    data = []
     f = open(nombre_fich, "r")
     #El siguiente booleano indica cuando empiezan los datos en el fichero.
     son_datos = False
@@ -13,3 +14,15 @@ def lecturaDatos(nombre_fich):
             son_datos=True
         if linea_data:
             data.append((linea.rstrip()).split(","))
+    return data
+
+#Calcula la distancia euclídea de e1 a e2
+def distanciaEuclidea(e1,e2):
+    distancia = 0
+    if len(e1)!=len(e2):
+        print("No se puede hallar la distancia euclídea porque hay diferente número de atributos.")
+    else:
+        for i in range(len(e1)):
+            distancia+=(e1[i]-e2[i])**2
+    distancia = math.sqrt(distancia)
+    return distancia
