856d36760b45618ded200538eb67c6fdda013112
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2018-05-27 12:33:24 +0200

    Sólo se ejecutan los nuevos algoritmos

--- Practica 3/resultados.py
+++ Practica 3/resultados.py
@@ -9,7 +9,8 @@ import enfriamiento_simulado
 import evolucion_diferencial
 
 FICHEROS_DATOS = ["../Data/ozone-320.arff","../Data/parkinsons.arff","../Data/spectf-heart.arff"]
-ALGORITMOS = ["KNN", "RELIEF", "BL", "GeneticoEstacionarioBLX", "GeneticoEstacionarioAritmetico", "GeneticoGeneracionalBLX", "GeneticoGeneracionalAritmetico", "Memetico prob=1 BLX", "Memetico prob=0.1 BLX", "Memetico prob=0.1 a los mejores BLX", "Memetico prob=1 Aritmetico", "Memetico prob=0.1 Aritmetico", "Memetico prob=0.1 a los mejores Aritmetico","ES","ILS","DE Rand1","DE CTB1"]
+#ALGORITMOS = ["KNN", "RELIEF", "BL", "GeneticoEstacionarioBLX", "GeneticoEstacionarioAritmetico", "GeneticoGeneracionalBLX", "GeneticoGeneracionalAritmetico", "Memetico prob=1 BLX", "Memetico prob=0.1 BLX", "Memetico prob=0.1 a los mejores BLX", "Memetico prob=1 Aritmetico", "Memetico prob=0.1 Aritmetico", "Memetico prob=0.1 a los mejores Aritmetico", "ES","ILS","DE Rand1","DE CTB1"]
+ALGORITMOS = ["ES","ILS","DE Rand1","DE CTB1"]
 NUM_PARTICIONES = 5
 
 for k in [1]:
@@ -20,6 +21,7 @@ for k in [1]:
     print("\n\n\n")
 
     for fichero in FICHEROS_DATOS:
+        '''
         res_1nn = knn.ValoracionKNN(fichero,k)
         print("Acabado KNN")
         res_relief = relief.ValoracionRelief(fichero,k)
@@ -46,6 +48,7 @@ for k in [1]:
         print("Acabado Memetico Aritmetico prob=0.1 sin mejores")
         res_memetico_01_mejores_aritmetico = memeticos.ValoracionMemetico(fichero,k,geneticos.cruceAritmetico,10,0.1,True)
         print("Acabado Memetico Aritmetico prob=0.1 con mejores")
+        '''
         res_es = enfriamiento_simulado.ValoracionEnfriamientoSimulado(fichero,k)
         print("Acabado Enfriamiento Simulado")
         res_ils = ils.ValoracionILS(fichero,k)
@@ -54,7 +57,8 @@ for k in [1]:
         print("Acabado DE Rand1")
         res_DE_CTB1 = evolucion_diferencial.ValoracionDECTB1(fichero,k)
         print("Acabado DE CTB1")
-        resultados = [res_1nn, res_relief, res_bl, res_genetico_estacionario_blx, res_genetico_estacionario_aritmetico, res_genetico_generacional_blx, res_genetico_generacional_aritmetico, res_memetico_1_blx, res_memetico_01_blx, res_memetico_01_mejores_blx, res_memetico_1_aritmetico, res_memetico_01_aritmetico, res_memetico_01_mejores_aritmetico, res_es, res_ils, res_DE_rand1, res_DE_CTB1]
+        #resultados = [res_1nn, res_relief, res_bl, res_genetico_estacionario_blx, res_genetico_estacionario_aritmetico, res_genetico_generacional_blx, res_genetico_generacional_aritmetico, res_memetico_1_blx, res_memetico_01_blx, res_memetico_01_mejores_blx, res_memetico_1_aritmetico, res_memetico_01_aritmetico, res_memetico_01_mejores_aritmetico, res_es, res_ils, res_DE_rand1, res_DE_CTB1]
+        resultados = [res_es, res_ils, res_DE_rand1, res_DE_CTB1]
         print("\n\n\n")
         print("############################################")
         print("Fichero de datos: " + fichero)
@@ -65,6 +69,7 @@ for k in [1]:
             for res in resultados[i]:
                 print(str(res[0][0]*2) + "-----" + str(res[0][1]*2) + "-----" + str(res[0][0]+res[0][1]) + "-----" + str(res[1]) + "\n")
 
+        '''
         media_tiempo_1nn = 0
         media_tc_1nn = 0
         media_tr_1nn = 0
@@ -169,6 +174,7 @@ for k in [1]:
             media_tr_memetico_01_mejores_aritmetico+=2*res[0][1]/NUM_PARTICIONES
             media_tiempo_memetico_01_mejores_aritmetico+=res[1]/NUM_PARTICIONES
 
+        '''
         media_tiempo_es = 0
         media_tc_es = 0
         media_tr_es = 0
@@ -204,6 +210,7 @@ for k in [1]:
         print("\n\n")
         print("################################################################")
         print("MEDIAS: \n")
+        '''
         print("1NN: " + str(media_tc_1nn) + " --- " + str(media_tr_1nn) + " --- " + str(0.5*media_tc_1nn+0.5*media_tr_1nn) + " --- " + str(media_tiempo_1nn) + "\n")
         print("RELIEF: " + str(media_tc_relief) + " --- " + str(media_tr_relief) + " --- " + str(0.5*media_tc_relief+0.5*media_tr_relief) + " --- " + str(media_tiempo_relief) + "\n")
         print("BL: " + str(media_tc_bl) + " --- " + str(media_tr_bl) + " --- " + str(0.5*media_tc_bl+0.5*media_tr_bl) + " --- " + str(media_tiempo_bl) + "\n")
@@ -217,8 +224,9 @@ for k in [1]:
         print("Memetico prob=1 Aritmetico: " + str(media_tc_memetico_1_aritmetico) + " --- " + str(media_tr_memetico_1_aritmetico) + " --- " + str(0.5*media_tc_memetico_1_aritmetico+0.5*media_tr_memetico_1_aritmetico) + " --- " + str(media_tiempo_memetico_1_aritmetico) + "\n")
         print("Memetico prob=0.1 Aritmetico: " + str(media_tc_memetico_01_aritmetico) + " --- " + str(media_tr_memetico_01_aritmetico) + " --- " + str(0.5*media_tc_memetico_01_aritmetico+0.5*media_tr_memetico_01_aritmetico) + " --- " + str(media_tiempo_memetico_01_aritmetico) + "\n")
         print("Memetico prob=0.1 a los mejores Aritmetico: " + str(media_tc_memetico_01_mejores_aritmetico) + " --- " + str(media_tr_memetico_01_mejores_aritmetico) + " --- " + str(0.5*media_tc_memetico_01_mejores_aritmetico+0.5*media_tr_memetico_01_mejores_aritmetico) + " --- " + str(media_tiempo_memetico_01_mejores_aritmetico) + "\n")
-        print("ES" + str(media_tc_es) + " --- " + str(media_tr_es) + " --- " + str(0.5*media_tc_es+0.5*media_tr_es) + " --- " + str(media_tiempo_es) + "\n")
-        print("ILS" + str(media_tc_ils) + " --- " + str(media_tr_ils) + " --- " + str(0.5*media_tc_ils+0.5*media_tr_ils) + " --- " + str(media_tiempo_ils) + "\n")
-        print("DE Rand1" + str(media_tc_DE_rand1) + " --- " + str(media_tr_DE_rand1) + " --- " + str(0.5*media_tc_DE_rand1+0.5*media_tr_DE_rand1) + " --- " + str(media_tiempo_DE_rand1) + "\n")
-        print("DE CTB1" + str(media_tc_DE_CTB1) + " --- " + str(media_tr_DE_CTB1) + " --- " + str(0.5*media_tc_DE_CTB1+0.5*media_tr_DE_CTB1) + " --- " + str(media_tiempo_DE_CTB1) + "\n")
+        '''
+        print("ES: " + str(media_tc_es) + " --- " + str(media_tr_es) + " --- " + str(0.5*media_tc_es+0.5*media_tr_es) + " --- " + str(media_tiempo_es) + "\n")
+        print("ILS: " + str(media_tc_ils) + " --- " + str(media_tr_ils) + " --- " + str(0.5*media_tc_ils+0.5*media_tr_ils) + " --- " + str(media_tiempo_ils) + "\n")
+        print("DE Rand1: " + str(media_tc_DE_rand1) + " --- " + str(media_tr_DE_rand1) + " --- " + str(0.5*media_tc_DE_rand1+0.5*media_tr_DE_rand1) + " --- " + str(media_tiempo_DE_rand1) + "\n")
+        print("DE CTB1: " + str(media_tc_DE_CTB1) + " --- " + str(media_tr_DE_CTB1) + " --- " + str(0.5*media_tc_DE_CTB1+0.5*media_tr_DE_CTB1) + " --- " + str(media_tiempo_DE_CTB1) + "\n")
         print("################################################################")
